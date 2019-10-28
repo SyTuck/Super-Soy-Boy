0c7039566308366864807da5ef86e10d14d2256b
JORDAN TUCK [Student] <jordan.tuck@sl.on.ca>
2019-10-28 15:37:43 -0400

    Jump control added

--- Assets/Scripts/SoyBoyController.cs
+++ Assets/Scripts/SoyBoyController.cs
@@ -36,20 +36,31 @@ public class SoyBoyController : MonoBehaviour
 {
     public float speed = 14f;
     public float accel = 6f;
+    public bool isJumping;
+    public float jumpSpeed = 8.0f;
+    public float jumpDurationThreshold = 0.25f;
+    
     private Vector2 input;
     private SpriteRenderer sr;
     private Rigidbody2D rb;
     private Animator animator;
+    private float rayCastLengthCheck = 0.005f;
+    private float width;
+    private float height;
+    private float jumpDuration;
 
     void Awake()
     {
-        sr = GetComponent<SpriteRenderer>();
-        animator = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody2D>();
+        sr = this.GetComponent<SpriteRenderer>();
+        animator = this.GetComponent<Animator>();
+        rb = this.GetComponent<Rigidbody2D>();
+        width = this.GetComponent<Collider2D>().bounds.extents.x + 0.1f;
+        height = this.GetComponent<Collider2D>().bounds.extents.y + 0.2f;
     }
 
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
 	
 	}
 
@@ -65,6 +76,33 @@ public class SoyBoyController : MonoBehaviour
         else if (input.x < 0f)
         {
             sr.flipX = true;
+        }
+
+        if (input.y >= 1.0f)
+        {
+            jumpDuration += Time.deltaTime;
+        }
+        else
+        {
+            isJumping = false;
+            jumpDuration = 0.0f;
+        }
+
+        if (PlayerIsOnGround() && isJumping == false)
+        {
+            if (input.y > 0.0f)
+            {
+                isJumping = true;
+            }
+        }
+
+        if (jumpDuration > jumpDurationThreshold)
+        {
+            input.y = 0.0f;
+        }
+        if (isJumping && jumpDuration < jumpDurationThreshold)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
         }
     }
 
@@ -83,5 +121,21 @@ public class SoyBoyController : MonoBehaviour
 
         rb.AddForce(new Vector2(((input.x * speed) - rb.velocity.x) * acceleration, 0));
         rb.velocity = new Vector2(xVelocity, rb.velocity.y);
+    }
+
+    public bool PlayerIsOnGround()
+    {
+        bool groundCheck1 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - height), -Vector2.up, rayCastLengthCheck);
+        bool groundCheck2 = Physics2D.Raycast(new Vector2(transform.position.x + (width - 0.2f), transform.position.y - height), -Vector2.up, rayCastLengthCheck);
+        bool groundCheck3 = Physics2D.Raycast(new Vector2(transform.position.x - (width - 0.2f), transform.position.y - height), -Vector2.up, rayCastLengthCheck);
+
+        if (groundCheck1 || groundCheck2 || groundCheck3)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
