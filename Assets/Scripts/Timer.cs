5782b6f6663520321b65c15d24f9c8e4ba6554d1
JORDAN TUCK [Student] <jordan.tuck@sl.on.ca>
2019-10-30 09:27:21 -0400

    Added the level timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private Text timerText;
+
+    private void Awake()
+    {
+        timerText = this.GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        timerText.text = System.Math.Round((decimal)Time.timeSinceLevelLoad, 2).ToString();
+    }
+
+}
